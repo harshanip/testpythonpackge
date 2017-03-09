f8289617e26c16dd5134dd2bc4423269a9bc162f
Perera, Harshani <Harshani.Perera@Pearson.com>
2017-03-09 15:50:38 +0530

    Commit module code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'tstpckhari',
+  packages = ['tstpckhari'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Harshani Perera',
+  author_email = 'perera.harshani@gmail.com',
+  url = 'https://github.com/harshanip/testpythonpackge', # use the URL to the github repo
+  download_url = 'https://github.com/harshanip/testpythonpackge.git/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
+
